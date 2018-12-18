c46657a23a8f251cc866d1afde012c8a7d25775d
jlacayo <jlacayorb@gmail.com>
2018-12-18 02:34:38 +0000

    About Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-   root 'welcome#index'
+     get '/about' => 'welcome#about'
+     root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
